576ea9fada8b87da9bb9d54d5fc25b47bbdc6132
YounesEssl <younes.esslimani@efrei.net>
2026-02-12 15:17:45 +0100

    feat: implement hand comparison

--- src/comparator/compare.ts
+++ src/comparator/compare.ts
@@ -1,1 +1,9 @@
-// Hand comparator - to be implemented
+import { HandResult } from '../models/types'
+
+export function compareHands(a: HandResult, b: HandResult): number {
+  if (a.category !== b.category) return a.category - b.category
+  for (let i = 0; i < Math.min(a.rankValues.length, b.rankValues.length); i++) {
+    if (a.rankValues[i] !== b.rankValues[i]) return a.rankValues[i] - b.rankValues[i]
+  }
+  return 0
+}
